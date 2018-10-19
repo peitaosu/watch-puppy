beea01126b1a826f15edc5e1f1df8de20cc7d292
Tony Su <peitaosu@163.com>
2018-10-19 22:56:55 +0800

    Object oriented

--- watch-fs.py
+++ watch-fs.py
@@ -4,21 +4,43 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
+class FileWatcher():
+    def __init__(self, path_to_watch="."):
+        self.path_to_watch = path_to_watch
+        self.event_handler = LoggingEventHandler()
+        self.observer = Observer()
+        self.recursive=True
+    
+    def set_log(self, log_file_path):
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            filename=log_file_path
+            )
+
+    def schedule(self):
+        self.observer.schedule(self.event_handler, self.path_to_watch, self.recursive)
+    
+    def start(self):
+        self.observer.start()
+    
+    def stop(self):
+        self.observer.stop()
+    
+    def join(self):
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.observer.stop()
+        self.observer.join()
+
 if __name__ == "__main__":
     path_to_watch = sys.argv[1] if len(sys.argv) > 1 else '.'
     log_file_path = sys.argv[2] if len(sys.argv) > 2 else None
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        filename=log_file_path
-                        )
-    event_handler = LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path_to_watch, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    watcher = FileWatcher(path_to_watch)
+    watcher.set_log(log_file_path)
+    watcher.schedule()
+    watcher.join()
