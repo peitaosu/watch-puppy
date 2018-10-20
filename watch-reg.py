6bfdfe6709e03228c7a6608dd855fffcd6920d26
Tony Su <peitaosu@163.com>
2018-10-20 23:41:33 +0800

    Object oriented

--- watch-reg.py
+++ watch-reg.py
@@ -3,41 +3,59 @@ import win32con
 import logging
 import sys
 
+
+class RegWatcher():
+
+    def __init__(self):
+        pass
+
+    def set_reg_root(self, reg_root):
+        reg_roots = {
+            "HKCU": win32con.HKEY_CURRENT_USER,
+            "HKLM": win32con.HKEY_LOCAL_MACHINE,
+            "HKCR": win32con.HKEY_CLASSES_ROOT
+        }
+        self.reg_root = reg_roots[reg_root]
+
+    def set_reg_key(self, reg_key):
+        self.reg_key = reg_key
+
+    def set_log(self, log_file_path):
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            filename=log_file_path
+        )
+        self.log = logging.getLogger()
+
+    def start(self):
+        values = {(self.reg_root, self.reg_key, "DateTime"): (win32con.REG_DWORD, 1),
+                  (self.reg_root, self.reg_key, "Templates"): (win32con.REG_DWORD, 0),
+                  (self.reg_root, self.reg_key, "UnitConv"): (win32con.REG_DWORD, 0)}
+
+        while True:
+            for (hive, key, value_name), (value_type, value) in values.iteritems():
+                handle_with_set_rights = win32api.RegOpenKeyEx(
+                    hive, key, 0, win32con.KEY_SET_VALUE)
+                self.log.info(r"Setting %s\%s\%s = %s" %
+                              (hive, key, value_name, value))
+                win32api.RegSetValueEx(
+                    handle_with_set_rights, value_name, 0, value_type, value)
+                win32api.RegCloseKey(handle_with_set_rights)
+
+            # Open and close the handle here as otherwise the set operation above
+            # will trigger a further round
+            handle_to_be_watched = win32api.RegOpenKeyEx(
+                self.reg_root, self.reg_key, 0, win32con.KEY_NOTIFY)
+            win32api.RegNotifyChangeKeyValue(
+                handle_to_be_watched, False, win32api.REG_NOTIFY_CHANGE_LAST_SET, None, False)
+            win32api.RegCloseKey(handle_to_be_watched)
+
+
 if __name__ == "__main__":
-    hive_switcher = {
-        'HKCU': win32con.HKEY_CURRENT_USER,
-        'HKLM': win32con.HKEY_LOCAL_MACHINE,
-        'HKCR': win32con.HKEY_CLASSES_ROOT
-    }
-
-    hive_to_watch = hive_switcher[sys.argv[1]] if len(
-        sys.argv) > 1 else win32con.HKEY_CURRENT_USER
-    key_to_watch = sys.argv[2] if len(sys.argv) > 2 else 'Software'
-    log_file_path = sys.argv[3] if len(sys.argv) > 3 else None
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        filename=log_file_path
-                        )
-    log = logging.getLogger()
-
-    values = {(hive_to_watch, key_to_watch, 'DateTime'): (win32con.REG_DWORD, 1),
-              (hive_to_watch, key_to_watch, 'Templates'): (win32con.REG_DWORD, 0),
-              (hive_to_watch, key_to_watch, 'UnitConv'): (win32con.REG_DWORD, 0)}
-
-    while True:
-        for (hive, key, value_name), (value_type, value) in values.iteritems():
-            handle_with_set_rights = win32api.RegOpenKeyEx(
-                hive, key, 0, win32con.KEY_SET_VALUE)
-            log.info(r'Setting %s\%s\%s = %s' % (hive, key, value_name, value))
-            win32api.RegSetValueEx(
-                handle_with_set_rights, value_name, 0, value_type, value)
-            win32api.RegCloseKey(handle_with_set_rights)
-
-        # Open and close the handle here as otherwise the set operation above
-        # will trigger a further round
-        handle_to_be_watched = win32api.RegOpenKeyEx(
-            hive_to_watch, key_to_watch, 0, win32con.KEY_NOTIFY)
-        win32api.RegNotifyChangeKeyValue(
-            handle_to_be_watched, False, win32api.REG_NOTIFY_CHANGE_LAST_SET, None, False)
-        win32api.RegCloseKey(handle_to_be_watched)
+    watcher = RegWatcher()
+    watcher.set_reg_root(sys.argv[1]) if len(sys.argv) > 1 else watcher.set_reg_root("HKCU")
+    watcher.set_reg_key(sys.argv[2]) if len(sys.argv) > 2 else watcher.set_reg_key("Software")
+    watcher.set_log(sys.argv[3]) if len(sys.argv) > 3 else watcher.set_log(None)
+    watcher.start()
